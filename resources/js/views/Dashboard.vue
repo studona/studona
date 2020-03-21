091d6970d5711bdf950ce1f3ec105f876dcc3c9e
Philipp Hirsch <itself@hanspolo.net>
2020-03-21 18:54:30 +0100

    Add a dashboard view and some component templates

---
+++ resources/js/views/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Dashboard</h1>
+
+        <h2>Interaktionen</h2>
+        <InteractionList></InteractionList>
+
+        <h2>Aufgaben</h2>
+        <TaskList></TaskList>
+    </div>
+</template>
+
+<script>
+    import InteractionList from '../components/InteractionList';
+    import TaskList from '../components/TaskList';
+
+    export default {
+        name: 'Dashboard',
+
+        components: {
+            InteractionList,
+            TaskList
+        }
+    }
+</script>
