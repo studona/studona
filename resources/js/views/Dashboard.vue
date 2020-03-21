294a21ab13b37eb4f1f91cfde1605d75f462db2f
Philipp Hirsch <itself@hanspolo.net>
2020-03-21 21:54:09 +0100

    a little more styling

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -1,17 +1,37 @@
 <template>
     <div>
-        <div class="page-header">
+        <div class="page-header white">
             <h1>{{ $t('views.dashboard') }}</h1>
-        </div>
-
-        <div class="card white">
-            <h2>{{ $t('concepts.interactions') }}</h2>
-            <InteractionList></InteractionList>
         </div>
 
         <div class="card blue">
             <h2>{{ $t('concepts.tasks') }}</h2>
+            <ul class="flex">
+                <li class="flex-1 text-left">
+                    <a class="text-gray-400 uppercase hover:text-gray-100" href="#">Ausstehend</a>
+                </li>
+                <li class="flex-1 text-center">
+                    <a class="text-gray-400 uppercase hover:text-gray-100" href="#">Erledigt</a>
+                </li>
+                <li class="flex-1 text-right">
+                    <a class="text-gray-400 uppercase hover:text-gray-100" href="#">Optional</a>
+                </li>
+            </ul>
+
+            <div class="mt-4 bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Keine offenen Aufgaben!</strong>
+                <span class="block sm:inline">Sehr gut, du hast alle Aufgaben für diese Woche bearbeitet.</span>
+            </div>
             <TaskList></TaskList>
+        </div>
+
+        <div class="card white">
+            <h2>{{ $t('concepts.interactions') }}</h2>
+            <div class="mt-4 bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Keine Nachrichten!</strong>
+                <span class="block sm:inline">Stelle Fragen oder hacke noch mal nach, falls du auf Antworten wartest.</span>
+            </div>
+            <InteractionList></InteractionList>
         </div>
 
         <div class="card orange">
