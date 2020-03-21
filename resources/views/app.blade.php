5f543e8517614a5c324509fec6d5defb63356e8e
TitusKirch <requests@tkirch.dev>
2020-03-21 22:27:13 +0100

    fix: remove old fa4

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -16,10 +16,6 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-
-        <!-- DEV -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
     <body>
         <div id="app">
