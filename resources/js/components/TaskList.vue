1d763ea2fc4a297c1f249331b75cd6d57c244558
Christian Stolz <hg8496@cstolz.de>
2020-03-22 21:54:03 +0100

    Added optional task functionality

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -3,13 +3,13 @@
         <h2>Tasks</h2>
         <ul class="flex">
             <li class="flex-1 text-left">
-                <a class="text-gray-400 uppercase hover:text-gray-100" v-on:click.stop.prevent="showOpen" href="#">Ausstehend</a>
+                <a class="text-gray-400 uppercase hover:text-gray-100" v-on:click.stop.prevent="type = 'open'" href="#">Ausstehend</a>
             </li>
             <li class="flex-1 text-center">
-                <a class="text-gray-400 uppercase hover:text-gray-100"  v-on:click.stop.prevent="showDone" href="#">Erledigt</a>
+                <a class="text-gray-400 uppercase hover:text-gray-100"  v-on:click.stop.prevent="type = 'done'" href="#">Erledigt</a>
             </li>
             <li class="flex-1 text-right">
-                <a class="text-gray-400 uppercase hover:text-gray-100" href="#">Optional</a>
+                <a class="text-gray-400 uppercase hover:text-gray-100" v-on:click.stop.prevent="type = 'optional'" href="#">Optional</a>
             </li>
         </ul>
 
@@ -17,11 +17,10 @@
             <i class="fas fa-spinner fa-spin"></i>
         </div>
         <div v-if="!errors.length">
-            <div class="mt-4 bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert" v-if="!loading && tasks && tasks.length===0">
-                <strong class="font-bold">Keine offenen Aufgaben!</strong>
-                <span class="block sm:inline">Sehr gut, du hast alle Aufgaben bearbeitet.</span>
+            <div class="mt-4 bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert" v-if="!loading && filteredTasks && filteredTasks.length===0">
+                <strong class="font-bold">Keine Aufgaben!</strong>
             </div>
-            <div v-if="tasks && tasks.length">
+            <div v-if="filteredTasks && filteredTasks.length">
                 <div class="task-list-item flex" v-for="task in filteredTasks" :key="task.name">
                     <Task v-bind:title="task.title" subject="Fach" v-bind:completed="task.completed" class="title"></Task>
                 </div>
@@ -63,15 +62,9 @@
                     return this.openTasks;
                 } else if(this.type === "done") {
                     return this.finishedTasks;
+                } else {
+                    return [];
                 }
-            }
-        },
-        methods: {
-            showOpen: function() {
-                this.type = "open";
-            },
-            showDone: function() {
-                this.type = "done";
             }
         },
         created() {
