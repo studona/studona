92fd53b060be3f82981fdaf87e3ee489e17a548e
leonie <feldbusl@informatik.uni-freiburg.de>
2020-03-22 15:46:36 +0100

    better topicpage

--- resources/js/views/Topicpage.vue
+++ resources/js/views/Topicpage.vue
@@ -3,32 +3,50 @@
         <!-- Main block -->
         <main>
             <!-- Content -->
-            <div class="page-header orange" id="topic_title">
-                <h1> Topic </h1>
+            <div class="page-header violet" id="topic_title">
+                <h1> Induktion </h1>
+                <h2> Physik, Klasse 7 </h2>
             </div>
 
             <div class="card white" id="topic_explanation">
-                <h2>Explanation</h2>
-                This is the text for the explanation of the topic.
+                <h2>Erklärung</h2>
+                <p>Die elektromagnetische Induktion ist ein Vorgang, bei dem durch Bewegung eines elektrischen Leiters
+                im Magnetfeld oder durch Änderung des von einem Leiter umschlossenen Magnetfeldes eine elektrische
+                Spannung und ein Stromfluss erzeigt werden.</p>
             </div>
 
-            <div class="card blue" id="topic_resources">
-                <h2>Resources</h2>
-                Here is the space for the resources of the learning e.g. videos etc.
+            <div class="card violet" id="topic_resources">
+                <h2>Material zum Thema</h2>
+                <div class="mt-4 bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">Kein Material!</strong>
+                    <span class="block sm:inline">Bisher ist kein Material zu diesem Thema hinterlegt.</span>
+                </div>
             </div>
 
             <div class="card white" id="topic_preconditions">
-                <h2>References</h2>
-                Here we can see the preconditions.
+                <h2>Voraussetzungen</h2>
+                <button class="btn btn-red">Magnetismus I</button>
+                <button class="btn btn-red">Das magnetische Feld</button>
+                <button class="btn btn-white">Formeln umstellen</button>
             </div>
 
-            <div class="card blue" id="topic_tasks">
-                <h2>Tasks</h2>
-                The space for the tasks.
+            <div class="card violet" id="topic_tasks">
+                <h2>Aufgaben</h2>
+                <div class="mt-4 bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">Keine Aufgaben!</strong>
+                    <span class="block sm:inline">Bisher sind keine Aufgaben zu diesem Thema hinterlegt.</span>
+                </div>
             </div>
 
-            <div class="card blue">
+            <div class="card red">
                 <h2>{{ $t('actions.rate') }}</h2>
+                <span class="block sm:inline">
+                    <div class="stern" style="float:left;margin:5px;"></div>
+                    <div class="stern" style="float:left;margin:5px;"></div>
+                    <div class="stern" style="float:left;margin:5px;"></div>
+                    <div class="stern" style="float:left;margin:5px;"></div>
+                    <div class="stern" style="float:left;margin:5px;"></div>
+                </span>
             </div>
         </main>
     </div>
@@ -43,4 +61,40 @@
 </script>
 
 <style scoped>
+    .btn {
+        @apply font-bold py-2 px-4 rounded;
+    }
+    .btn-red {
+        @apply bg-red-500 text-white border border-white;
+    }
+    .btn-red:hover {
+        @apply bg-red-700;
+    }
+    .btn-white {
+        @apply bg-transparent text-red-500 border border-red-500;
+    }
+    .btn-white:hover {
+        @apply bg-red-700;
+    }
+
+    div.stern {
+        width: 0;
+        height: 0;
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        border-bottom: 20px solid #ffc800;
+        position: relative;
+    }
+
+    div.stern:after {
+        width: 0;
+        height: 0;
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        border-top: 20px solid #ffc800;
+        position: absolute;
+        content: "";
+        top: 7px;
+        left: -10px;
+    }
 </style>
