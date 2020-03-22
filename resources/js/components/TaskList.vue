1378dda8493df03dbe5f45b1253a3115d16c4432
Christian Stolz <hg8496@cstolz.de>
2020-03-22 16:36:06 +0100

    Spinner on tasklist

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
-        <div class="mt-4 bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert" v-if="!tasks || tasks && tasks.length===0">
-            <strong class="font-bold">Keine offenen Aufgaben!</strong>
-            <span class="block sm:inline">Sehr gut, du hast alle Aufgaben bearbeitet.</span>
+        <div class="fa-3x" v-if="loading">
+            <i class="fas fa-spinner fa-spin"></i>
         </div>
-        <div class="task-list" v-if="tasks && tasks.length">
+        <div v-if="!errors.length">
+            <div class="mt-4 bg-gray-100 border border-gray-400 text-gray-700 px-4 py-3 rounded relative" role="alert" v-if="!loading && tasks && tasks.length===0">
+                <strong class="font-bold">Keine offenen Aufgaben!</strong>
+                <span class="block sm:inline">Sehr gut, du hast alle Aufgaben bearbeitet.</span>
+            </div>
+            <div class="task-list" v-if="tasks && tasks.length">
             <div class="task-list-item flex" v-for="task in tasks" :key="task.name">
                 <div class="sm:w-1/6 md:w-1/12"><span class="label">Fach</span></div>
                 <div class="sm:w-5/6 md:w-11/12">
@@ -16,6 +20,12 @@
                 </div>
             </div>
         </div>
+        </div>
+        <ul v-else>
+            <li v-for="(error, index) in errors" :key="index">
+                {{ error }}
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -25,6 +35,7 @@
 
         data() {
             return {
+                loading: true,
                 tasks: [],
                 errors: []
             }
@@ -37,10 +48,12 @@
         created() {
             axios.get(`http://jsonplaceholder.typicode.com/todos?userId=1`)
                 .then(response => {
-                this.tasks = response.data
+                this.tasks = response.data;
+                this.loading = false;
             })
             .catch(e => {
                 this.errors.push(e)
+                this.loading = false;
             });
         }
     }
