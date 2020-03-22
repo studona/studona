1624bdc772673410fbfec378e52f73ea394c6a35
TitusKirch <requests@tkirch.dev>
2020-03-22 10:56:41 +0100

    feat: build store js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,19 @@
 import Vue from 'vue';
+import Vuex from 'vuex'
 import i18n from './i18n';
 import router from './router';
+import store from './store'
 import App from './components/App';
 
 require('./bootstrap');
 
 const app = new Vue({
     i18n,
+    router,
+    store,
     el: '#app',
     productionSourceMap: false,
     components: {
         App
     },
-    router
 });
